7eb7f9a790dcbc2905e9b926475a27a98cd21760
pascal-la <lapascal89@gmail.com>
2024-09-30 18:20:25 +0200

    add zod

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,26 +1,50 @@
 import { fail, redirect, type Actions } from "@sveltejs/kit";
+import { zfd } from "zod-form-data";
 
 export const actions: Actions = {
+	// default: async ({ request }) => {
+	// 	const formData = await request.formData();
+	// 	const user = String(formData.get("user"));
+	// 	const password = String(formData.get("password"));
+
+	// 	const errors: Record<string, unknown> = {};
+
+	// 	if (!user || typeof user !== "string") {
+	// 		errors.user = "required";
+	// 	}
+
+	// 	if (!password || typeof password !== "string") {
+	// 		errors.password = "required";
+	// 	}
+
+	// 	if (Object.keys(errors).length > 0) {
+	// 		const data = {
+	// 			data: Object.fromEntries(formData),
+	// 			errors
+	// 		};
+	// 		return fail(400, data);
+	// 	}
+
+	// 	throw redirect(303, "/todos");
+	// }
+
+	//? using Zod
 	default: async ({ request }) => {
 		const formData = await request.formData();
-		const user = String(formData.get("user"));
-		const password = String(formData.get("password"));
 
-		const errors: Record<string, unknown> = {};
+		const loginSchema = zfd.formData({
+			user: zfd.text(),
+			password: zfd.text()
+		});
 
-		if (!user || typeof user !== "string") {
-			errors.user = "required";
-		}
+		const result = loginSchema.safeParse(formData);
 
-		if (!password || typeof password !== "string") {
-			errors.password = "required";
-		}
-
-		if (Object.keys(errors).length > 0) {
+		if (!result.success) {
 			const data = {
 				data: Object.fromEntries(formData),
-				errors
+				errors: result.error.flatten().fieldErrors
 			};
+
 			return fail(400, data);
 		}
 
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
 	import { enhance } from "$app/forms";
+	import type { ActionData } from "./$types";
+
+	export let form: ActionData;
 </script>
+
+<pre>
+	{JSON.stringify(form, null, 2)}
+</pre>
 
 <h1>Login</h1>
 
 <form method="POST" use:enhance>
-	<input type="text" name="user" />
+	<input type="text" name="user" value={form?.data?.user ?? ""} />
+	{#if form?.errors?.user}
+		<p class="error">Name is required</p>
+	{/if}
 	<input type="password" name="password" />
+	{#if form?.errors?.password}
+		<p class="error">Password is required</p>
+	{/if}
 	<button type="submit"> Login </button>
 </form>
+
+<style>
+	.error {
+		color: tomato;
+	}
+</style>
